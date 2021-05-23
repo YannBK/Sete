e53ae68e7af0a84855d1f9a2639c79d1a93a9082
yann <yann.bettker@gmail.com>
2021-05-23 18:49:53 +0200

    harmonisation

--- js/script_index.js
+++ js/script_index.js
@@ -1,17 +1,20 @@
 
 const region = document.querySelector('#laRegion');
 const regions = document.getElementsByClassName('region');
+const btnRegion = document.getElementById('laRegion');
 
 //liste déroulente et hover sur les liens touristiques
 const hoverRegions = () => {
     for (let i = 0; i < regions.length; i++) {
         regions[i].style.display = "inline-block";
+        btnRegion.classList.add('page1');
     }
 }
 
 const removeHoverRegions = () => {
     for (let i = 0; i < regions.length; i++) {
         regions[i].style.display = "none";
+        btnRegion.classList.remove('page1');
     }
 }
 
