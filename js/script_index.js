f3cd3cf97b02859cc35e3967cd8c3538a598403a
yann <yann.bettker@gmail.com>
2021-07-07 14:31:27 +0200

    diapo+plan fullscreen

--- js/script_index.js
+++ js/script_index.js
@@ -1,5 +1,5 @@
 
-// ------------------ activer / désactiver class
+//! ------------------ activer / désactiver class
 let current = document.getElementsByClassName(" active");
 const btnAccueil = document.getElementById('btnAccueil')
 const btnDescription = document.getElementById('btnDescription')
@@ -7,6 +7,7 @@ const btnPlan = document.getElementById('btnPlan')
 const btnTarifs = document.getElementById('btnTarifs')
 const btnRegion = document.getElementById('btnRegion')
 const btnContact = document.getElementById('btnContact');
+const btnNav = document.getElementsByClassName('lienNav')
 
 function removeActive() {
 	while (current.length > 0) {
@@ -72,8 +73,8 @@ function scrollBtn() {
 window.addEventListener('scroll', scrollBtn)
 window.addEventListener('resize', scrollBtn)
 
-// ------------------ Diapo 
-//Array d'images
+//! ------------------ Diapo 
+//*Array d'images
 const Images = [
 	"Fichiers/images/01_Vue_mer.JPG",
 	"Fichiers/images/02_salon_vue_entree.JPG",
@@ -101,8 +102,7 @@ const Images = [
 ];
 let arrLen = Images.length - 1
 
-//changement automatique + manuel
-
+//*changement automatique + manuel
 let speed = 3000;
 let i = 0;
 let b;
@@ -130,32 +130,41 @@ function manualSlide(direction) {
 		if (i < 0) {
 			i = arrLen
 		}
-		document.Slide.src = Images[i]
+		document.Slide.src = Images[i];
+		document.Slide2.src = Images[i];
 	}
 }
 
-//arreter le defilement
+//*arreter le defilement
 function arret() {
 	clearTimeout(objet_timer);
 	play.disabled = false;
 }
 
-//boutons de commande
+//*boutons de commande
 const prevBtn = document.getElementById('prev-btn');
+const prevBtn2 = document.getElementById('prev-btn2');
 prevBtn.addEventListener('click', () => manualSlide(-1));
+prevBtn2.addEventListener('click', () => manualSlide(-1));
 
 const nextBtn = document.getElementById('next-btn');
+const nextBtn2 = document.getElementById('next-btn2');
 nextBtn.addEventListener('click', () => manualSlide(1));
+nextBtn2.addEventListener('click', () => manualSlide(1));
 
 const stop = document.getElementById('stop');
+// const stop2 = document.getElementById('stop2');
 stop.addEventListener('click', () => arret());
+// stop2.addEventListener('click', () => arret());
 
 const play = document.getElementById('play');
+// const play2 = document.getElementById('play2');
 play.addEventListener('click', () => autoSlide('Slide'));
+// play2.addEventListener('click', () => autoSlide('Slide2'));
 
 autoSlide('Slide')
 
-// effet hover sur l'image
+//* effet hover sur l'image (fait apparaitre boutons)
 const divHover = document.querySelector('#divSlide');
 const buttonDiv = document.getElementsByClassName('btn');
 
@@ -174,23 +183,71 @@ const removeHoverDiv = () => {
 divHover.addEventListener("mouseenter", hoverDiv)
 divHover.addEventListener("mouseleave", removeHoverDiv)
 
-// Switch plein écran
-function fullscreen(element){
-	return (element.requestFullscreen ||
-		element.webkitRequestFullscreen ||
-		element.mozRequestFullScreen ||
-		element.msRequestFullscreen).call(element);
+//! Switch plein écran
+
+//* variables
+const smallSlide = document.getElementById('Slide');
+const Slide2 = document.getElementById('Slide2')
+const divaphoto = document.getElementById('divaphoto')
+const divaopacité = document.getElementById('divaopacité')
+
+const imgPlan = document.getElementById('imgPlan')
+const imgPlan2 = document.getElementById('imgPlan2')
+const divfullplan = document.getElementById('divfullplan')
+const divopacitéplan = document.getElementById('divopacitéplan')
+
+//*fonction switch
+function switchImage(elm1, elm2) {
+	if (window.innerWidth > 1000) {
+		if (elm1.style.display == 'none' && elm2.style.display == 'none') {//2em condition pas nécessaire
+			elm1.style.display = 'block';
+			elm2.style.display = 'block';
+		}
+		else {
+			elm1.style.display = 'none';
+			elm2.style.display = 'none';
+		}
 	}
+}
 
-let fullPlan = document.getElementById('imgPlan');
+//*eventlistener pour switch diapo
+smallSlide.addEventListener('click', () => switchImage(divaphoto, divaopacité))
+Slide2.addEventListener('click', () => switchImage(divaphoto, divaopacité))
 
-fullPlan.addEventListener('click', function(){
-	fullscreen(fullPlan);
-})
+//*eventlistener pour switch plan
+imgPlan.addEventListener('click', () => switchImage(divfullplan, divopacitéplan))
+imgPlan2.addEventListener('click', () => switchImage(divfullplan, divopacitéplan))
 
-let fullSlide = document.getElementById('divSlide');
-let smallSlide = document.getElementById('Slide');
+//*enleve fullscreen quand clique sur liennav
+function displayNone() {
+	divaphoto.style.display = 'none';
+	divaopacité.style.display = 'none';
+	divfullplan.style.display = 'none';
+	divopacitéplan.style.display = 'none';
+}
 
-smallSlide.addEventListener('click', function(){
-	fullscreen(fullSlide);
-})
+function display() {
+	const btnNavLength = btnNav.length;
+	for (let i = 0; i < btnNavLength - 1; i++) {
+		btnNav[i].addEventListener('click', displayNone);
+	}
+}
+display();
+
+//*enlève curseur zoom si width<1000
+function zoom(){
+	let width = document.documentElement.clientWidth;
+	if(width > 1000){
+		smallSlide.classList.add('zoom');
+		imgPlan.classList.add('zoom');
+	}
+	if(width < 1000){
+		smallSlide.classList.remove('zoom');
+		imgPlan.classList.remove('zoom');
+	}
+}
+zoom();
+window.addEventListener('resize', zoom)
+
+//*pour éviter de faire un double click la première fois qu'on zoom
+document.onload(displayNone())
